a65fb58b278d395eaf67d7347beecd85fd9d1dd2
Brian Tong <btong34@gmail.com>
2013-11-20 15:56:36 -0600

    include uploader directory and add CW settings

--- sinatra_skeleton/config/environment.rb
+++ sinatra_skeleton/config/environment.rb
@@ -35,6 +35,12 @@ APP_NAME = APP_ROOT.basename.to_s
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'uploader', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+# CarrierWave config 
+CarrierWave.configure do |config|
+    config.root = APP_ROOT + 'public/'
+end
