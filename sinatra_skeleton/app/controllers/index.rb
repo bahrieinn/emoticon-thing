1639d63147e7b0b6f8bc46a99a4486543ab7c6f7
Brian Tong <btong34@gmail.com>
2013-11-20 15:10:33 -0600

    first commit

---
+++ sinatra_skeleton/app/controllers/index.rb
@@ -0,0 +1,16 @@
+require 'cgi'
+
+get '/' do
+  # Look in app/views/index.erb
+  haml :index
+end
+
+get '/search' do
+
+  redirect "/" + CGI.escape(params[:emoticon])
+end
+
+get '/:emoticon' do
+  @escaped_emoticon = CGI.escape(params[:emoticon])
+  haml :result
+end
