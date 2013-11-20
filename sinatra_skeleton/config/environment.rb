1b236c9295d2b9374435f389df7a603eaa749699
Brian Tong <btong34@gmail.com>
2013-11-20 15:45:14 -0600

    add required gems

--- sinatra_skeleton/config/environment.rb
+++ sinatra_skeleton/config/environment.rb
@@ -20,6 +20,13 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'cgi'
+
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+require 'mini_magick'
+
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
