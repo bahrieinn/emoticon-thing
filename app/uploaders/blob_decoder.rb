1b2b51ad6f5e514b3ef7b34ad81cd13ebe92b7f2
Brian Tong <btong34@gmail.com>
2013-12-04 16:03:50 -0600

    fix math rand

--- app/uploaders/blob_decoder.rb
+++ app/uploaders/blob_decoder.rb
@@ -1,6 +1,6 @@
 class CarrierStringIO < StringIO
   def original_filename
-    "#{Time.now}#{Math.rand(99999)}.jpeg"
+    "#{Time.now}#{rand(99999)}.jpeg"
   end
   def content_type
     "image/jpeg"
